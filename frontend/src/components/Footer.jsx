a5fd70fad9b240f063e1180e5f8c311cd67ec241
Crutarth <crutarth@gmail.com>
2026-07-02 18:01:23 +0530

    ClickMart:Backend & Frontend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-4 border-t flex flex-col items-center text-sm text-gray-600 text-center">
+      <p className="flex flex-col items-center gap-1">
+        Developed with <span className="text-red-500">❤️</span> by{" "}
+        <a
+          href="#"
+          target="_blank"
+          className="text-blue-600 hover:underline"
+        >
+          Krutarth Katrodiya
+        </a>
+      </p>
+
+      <div className="flex flex-col items-center gap-1 mt-2">
+        <a
+          href="mailto:krutarthkatrodiya209@gmail.com"
+          className="text-blue-600 hover:underline"
+        >
+          krutarthkatrodiya209@gmail.com{" "}
+        </a>
+
+        <span className="flex items-center gap-1 text-gray-500 text-sm">
+          <i className="bi bi-geo-alt-fill" style={{ fontSize: "20px" }}></i>
+          Surat, India
+        </span>
+      </div>
+    </footer>
+  );
+}
+
